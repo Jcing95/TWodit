98d7fb7a0b586f2f3c3f61432ff9aa05f922b6d2
Jcing <jasin95@googlemail.com>
2019-03-16 14:35:19 +0100

    camera movement

--- src/main/java/de/Jcing/engine/gl/Renderer.java
+++ src/main/java/de/Jcing/engine/gl/Renderer.java
@@ -1,8 +1,12 @@
 package de.jcing.engine.gl;
 
+import java.util.LinkedList;
+
 import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL30;
 
+import de.jcing.engine.io.KeyBoard;
 import de.jcing.utillities.log.Log;
 import de.jcing.utillities.task.Task;
 import de.jcing.window.OpenGLWindow;
@@ -19,22 +23,37 @@ public class Renderer {
 	private static final float Z_NEAR = 0.01f;
 
 	private static final float Z_FAR = 1000.f;
+	
+	private static final float size = 0.5f;
 
 	private OpenGLWindow window;
 
 	private TestShader shader;
 
 	private Transformation transformation;
-
-	private GameItem testItem;
 	
 	private float rot;
 	
 	private Task rotator;
+	
+	private Camera camera;
+	
+	private LinkedList<GameItem> items;
 
 	public Renderer(OpenGLWindow win) {
 		this.window = win;
 		transformation = new Transformation();
+		camera = new Camera();
+		new Task(() -> {
+			if(KeyBoard.isPressed(GLFW.GLFW_KEY_W))
+				camera.movePosition(0, 0.1f, 0);
+			if(KeyBoard.isPressed(GLFW.GLFW_KEY_A))
+				camera.movePosition(-0.1f, 0, 0);
+			if(KeyBoard.isPressed(GLFW.GLFW_KEY_S))
+				camera.movePosition(0, -0.1f, 0);
+			if(KeyBoard.isPressed(GLFW.GLFW_KEY_D))
+				camera.movePosition(0.1f, 0, 0);
+		}).repeat(Task.perSecond(20)).start();
 		init();
 	}
 
@@ -48,10 +67,10 @@ public class Renderer {
 			shader.createUniform("texture_sampler");
 			Texture tex = new Texture("/gfx/terrain/cobble0.png");
 			float[] positions = new float[] 
-					{ -0.5f, 0.5f, -1.05f, 
-					-0.5f, -0.5f, -1.05f,
-					0.5f, -0.5f, -1.05f,
-					0.5f, 0.5f, -1.05f, };
+					{ -size, size, -1.05f, 
+					-size, -size, -1.05f,
+					size, -size, -1.05f,
+					size, size, -1.05f, };
 
 			float[] texCoords = new float[] {
 					0.0f, 1.0f,
@@ -61,15 +80,39 @@ public class Renderer {
 					};
 
 			int[] indices = new int[] { 0, 1, 3, 3, 1, 2, };
-			testItem = new GameItem(new Mesh(positions, texCoords, indices, tex));
-			testItem.setPosition(0, 0, -1);
-
+			Mesh mesh = new Mesh(positions, texCoords, indices, tex);
+			GameItem item1 = new GameItem(mesh);
+			item1.setPosition(-1, -1, -1);
+			GameItem item2 = new GameItem(mesh);
+			item2.setPosition(-1, 0, -1);
+			GameItem item3 = new GameItem(mesh);
+			item3.setPosition(0, -1, -1);
+			GameItem item4 = new GameItem(mesh);
+			item4.setPosition(0, 0, -1);
+			GameItem item5 = new GameItem(mesh);
+			item5.setPosition(1, 0, -1);
+			GameItem item6 = new GameItem(mesh);
+			item6.setPosition(0, 1, -1);
+			GameItem item7 = new GameItem(mesh);
+			item7.setPosition(1, 1, -1);
+			GameItem item8 = new GameItem(mesh);
+//			item8.setPosition(1, 1, -1);
+			items = new LinkedList<>();
+			items.add(item1);
+			items.add(item2);
+			items.add(item3);
+			items.add(item4);
+			items.add(item5);
+			items.add(item6);
+			items.add(item7);
+			items.add(item8);
+			
 			GL30.glViewport(0, 0, window.getWidth(), window.getHeight());
 			
 			
 			rotator = new Task(() -> {
-				rot+= 0.2;
-				testItem.setRotation(0, 0, rot);
+				rot += 0.2;
+				camera.setRotation(0, 0, rot);
 			}).repeat(10).start();
 		});
 
@@ -82,14 +125,16 @@ public class Renderer {
 			// update transformation
 			Matrix4f projectionMatrix = transformation.getProjectionMatrix(FOV, window.getWidth(), window.getHeight(),
 					Z_NEAR, Z_FAR);
+			Matrix4f viewMatrix = transformation.getViewMatrix(camera);
 			shader.setUniform("projectionMatrix", projectionMatrix);
-			Matrix4f worldMatrix = transformation.getWorldMatrix(testItem.getPosition(), testItem.getRotation(),
-					testItem.getScale());
-			shader.setUniform("worldMatrix", worldMatrix);
 			shader.setUniform("texture_sampler", 0);
-			// Draw the mesh
-			testItem.getMesh().render();
-
+			
+			for(GameItem item : items) {
+				Matrix4f modelViewMatrix = transformation.getModelViewMatrix(item, viewMatrix);
+				shader.setUniform("worldMatrix", modelViewMatrix);
+				// Draw the mesh
+				item.getMesh().render();
+			}
 			shader.unbind();
 		});
 		
@@ -98,7 +143,8 @@ public class Renderer {
 	public void finish() {
 		rotator.stop();
 		window.runInContext(() -> {
-			testItem.getMesh().cleanUp();
+			for(GameItem item : items)
+				item.getMesh().cleanUp();
 		});
 	}
 
